eaf5c7ae8be4187fbd7ffb836a45bd89384de73c
lfgberg <lfgaberg@gmail.com>
2022-11-22 16:20:53 -0500

    sort by descending CPU usage

--- automation-lab/task3.sh
+++ automation-lab/task3.sh
@@ -5,4 +5,4 @@ LOG=/var/log/testlogs/crypto
 DATE=`date +%m%d%y`
 
 # get the line with the highest cpu, output to log
-ps aux --sort pcpu | awk 'NR==2' >> $LOG.$DATE
+ps aux --sort -pcpu | awk 'NR==2' >> $LOG.$DATE
